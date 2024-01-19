045ce113b939cc47eea6b39f5bd4f7fc7aa28322
natidever <natnael7820@gmail.com>
2024-01-19 17:49:00 +0300

    fs

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { quotes } from '../assets'
-import {feedback} from '../constants'
+import {features, feedback} from '../constants'
 function Testimonial() {
   return (
    <section className='flex flex-col'>
@@ -10,12 +10,28 @@ function Testimonial() {
       <p className={`${styles.paragraph } mt-10 mr-40`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
     </div>
 
-    <div className="flex flex-col p-7">
-
+      <div className="flex flex-row gap-5">
       {feedback.map((feedbacks,index)=>(
-        feedbacks.title
-        
-      ))}
+         
+        <div className="flex flex-col w-[330px] h-[355px] bg-black-gradient rounded-[20px] relative">
+        <img src={quotes} alt="" className="w-[42px] h-[27.6pxx] mt-20 ml-5" />
+        <p className={`${styles.paragraph} p-5`}>{ feedbacks.content }</p>
+        <div className="flex flex-row pr-2">
+          <img src={feedbacks.img} className="w-[48px] h-[48px] absolute md:bottom-10 md:left-5" />
+          <div className="md:relative md:top-[40px] md:left-[100px] md:bottom-[-50px] flex flex-col text-white ">
+            <h2>{feedbacks.name}</h2>
+            <p>{feedbacks.title}</p>
+          </div>
+        </div>
+      </div>
+     
+    
+
+  ))
+  }
+
+      </div>
+    
 
 
 
@@ -23,7 +39,6 @@ function Testimonial() {
 
     
 
-    </div>
    </section>
   )
 }
